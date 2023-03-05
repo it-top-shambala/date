2604d34e72b3d7a7ba5fb7f9cbe8129b184b729a
Starinin Andrey <starinin-andrey@ya.ru>
2023-03-05 11:59:11 +0300

    Update Date

--- lib/date/Date.cpp
+++ lib/date/Date.cpp
@@ -12,24 +12,47 @@ Date::Date(int year, int month, int day) {
     this->day = day;
 }
 Date::Date(string date) {
+    InitM(date);
+}
+Date::Date(string date, char format) {
+    switch (format) {
+        case 'M': //основной формат - dd.mm.yyyy
+            InitM(date);
+            break;
+        case 'S': //обратный формат - yyyy.mm.dd
+            InitS(date);
+            break;
+    }
+}
+
+void Date::InitM(string date) {
     vector<string> temp = Parser::ParserString(date, '.');
     this->day = stoi(temp[0]);
     this->month = stoi(temp[1]);
     this->year = stoi(temp[2]);
 }
-Date::Date(string date, char format) {
+void Date::InitS(string date) {
+    vector<string> temp = Parser::ParserString(date, '.');
+    this->day = stoi(temp[2]);
+    this->month = stoi(temp[1]);
+    this->year = stoi(temp[0]);
+}
+
+string Date::ToString(char format) {
     switch (format) {
-        case 'M': //основной формат - dd.mm.yyyy
-            vector<string> temp = Parser::ParserString(date, '.');
-            this->day = stoi(temp[0]);
-            this->month = stoi(temp[1]);
-            this->year = stoi(temp[2]);
-            break;
-        case 'S': //обратный формат - yyyy.mm.dd
-            vector<string> temp = Parser::ParserString(date, '.');
-            this->day = stoi(temp[2]);
-            this->month = stoi(temp[1]);
-            this->year = stoi(temp[0]);
-            break;
+        case 'M':
+            return ToStringM();
+        case 'S':
+            return ToStringS();
     }
+}
+string Date::ToString() {
+    return ToStringM();
+}
+
+string Date::ToStringM() { //основной формат - dd.mm.yyyy
+    return to_string(this->day) + "." + to_string(this->month) + "." + to_string(this->year);
+}
+string Date::ToStringS() { //обратный формат - yyyy.mm.dd
+    return to_string(this->year) + "." + to_string(this->month) + "." + to_string(this->day);
 }
--- lib/date/Date.h
+++ lib/date/Date.h
@@ -15,4 +15,14 @@ public:
     Date(int year, int month, int day);
     Date(string date);
     Date(string date, char format);
+
+    string ToString(char format);
+    string ToString();
+
+private:
+    void InitM(string date); //основной формат - dd.mm.yyyy
+    void InitS(string date); //обратный формат - yyyy.mm.dd
+
+    string ToStringM();
+    string ToStringS();
 };
